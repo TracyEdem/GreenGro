b18e686c52fb7d8882ec688553547e77b8d85835
Tracy Edem <74256126+TracyEdem@users.noreply.github.com>
2021-12-10 22:43:39 +0000

    Add files via upload

---
+++ WebTechProject/model/dbcredentials.php
@@ -0,0 +1,8 @@
+<?php
+//define case sensitive constants with corresponding values for database details
+define("Server", "20.108.182.104");
+define("Username", "root");
+define("Password", "mydbp@ssword");
+//define("Password", "");
+define("Database","greengro");
+?>
