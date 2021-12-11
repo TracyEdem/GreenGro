6f8334ce6c3dee3b3aed8cc20f37fc70d2b2f05b
Tracy Edem <74256126+TracyEdem@users.noreply.github.com>
2021-12-11 20:19:23 +0000

    Update home.php

--- WebTechProject/view/home.php
+++ WebTechProject/view/home.php
@@ -26,7 +26,14 @@ if(isset($_SESSION['email'])) {
   <?php
     include_once("navbar.php");
     ob_start();
-    include "../model\dbconnect.php";
+    $conn = new mysqli("localhost", "root", "mydbp@ssword", "greengro");
+    // Check if connection is successful
+    if($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error."<br>");
+      }
+    else{  
+      echo "Successful connection!<br>";
+    }
     ob_end_clean();
    
     ?>
