805e5837a249693094874c0d63003e33aeee90da
Tracy Edem <74256126+TracyEdem@users.noreply.github.com>
2021-12-11 15:01:42 +0000

    Update dbcredentials.php

--- WebTechProject/model/dbcredentials.php
+++ WebTechProject/model/dbcredentials.php
@@ -1,8 +1,7 @@
 <?php
 //define case sensitive constants with corresponding values for database details
-define("Server", "20.108.182.104");
+define("Server", "localhost");
 define("Username", "root");
 define("Password", "mydbp@ssword");
-//define("Password", "");
 define("Database","greengro");
 ?>
